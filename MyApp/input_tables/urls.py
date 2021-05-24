60c6094e70736ba0b60ce3262dc2982fe55bfdea
Evgeny Andreev <tomlitke@mail.ru>
2021-05-24 17:07:51 +0300

    Deleting unnecessary files and changing page addressing

--- MyApp/input_tables/urls.py
+++ MyApp/input_tables/urls.py
@@ -7,11 +7,11 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('tables/<int:pk>/', views.delete_tables, name='delete_tables'),
-    path('tables/excel-to-table/', views.excel_to_table, name='to_table'),
+    path('<int:pk>/', views.delete_tables, name='delete_tables'),
+    path('excel-to-table/', views.excel_to_table, name='to_table'),
 
-    path('tables/', views.TableListView.as_view(), name='table_list'),
-    path('tables/upload/', views.UploadTableView.as_view(), name='class_upload_table'),
+    path('', views.TableListView.as_view(), name='table_list'),
+    path('upload/', views.UploadTableView.as_view(), name='class_upload_table'),
 ]
 
 if settings.DEBUG:
